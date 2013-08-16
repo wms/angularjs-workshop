5f031ee612cb80e65e474fd8a118aa8ca74191b2
Warren Seymour <wms@fountainhead-solutions.net>
2013-08-16 15:08:33 +0100

    Add couchdb 1.3 to vm

--- Vagrantfile
+++ Vagrantfile
@@ -5,4 +5,5 @@ Vagrant.configure("2") do |config|
   config.vm.box = "precise32"
   config.vm.provision :shell, :path => "_build/provision.sh"
   config.vm.network :forwarded_port, host: 8080, guest: 80
+  config.vm.network :forwarded_port, host: 8081, guest: 5984
 end
--- _build/provision.sh
+++ _build/provision.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+cp /vagrant/_build/sources.list /etc/apt/sources.list.d/
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6D3315B
+
 apt-get update
-apt-get install -y apache2
+apt-get install -y apache2 couchdb
 rm -rf /var/www
 ln -fs /vagrant /var/www
+cp /vagrant/_build/couchdb/local.ini /etc/couchdb/
+service couchdb restart
