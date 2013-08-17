fead9f9912705976ae00aad3227f1b684df6ccbf
Warren Seymour <wms@fountainhead-solutions.net>
2013-08-17 11:02:10 +0100

    Bootstrap couchdb databases

--- _build/provision.sh
+++ _build/provision.sh
@@ -4,8 +4,13 @@ cp /vagrant/_build/sources.list /etc/apt/sources.list.d/
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6D3315B
 
 apt-get update
-apt-get install -y apache2 couchdb
+apt-get install -y apache2 couchdb curl
 rm -rf /var/www
 ln -fs /vagrant /var/www
 cp /vagrant/_build/couchdb/local.ini /etc/couchdb/
+
 service couchdb restart
+
+while ! nc -z localhost 5984; do sleep 1; done
+curl -s -X PUT http://localhost:5984/cars
+curl -s -X PUT http://localhost:5984/todos
